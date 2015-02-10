93a20167c98e1cebe9dbf56e91e389fa71d47f80
Patrick McCarthy <p.t.mccarthy@gmail.com>
2015-02-10 00:10:25 -0800

    Add spy example test

--- test/spec/example_background_spec.js
+++ test/spec/example_background_spec.js
@@ -5,10 +5,17 @@ var App = require('../../src/chrome/background');
 // a trivial example test
 describe('Sample test', function() {
 
-  App.initialize();
+  beforeAll(function() {
+    spyOn(window.chrome.runtime.onInstalled, 'addListener');
+
+    App.initialize();
+  });
 
   it('Should have a test variable set to 7', function() {
     expect(App.testVariable).toEqual(7);
   });
 
+  it('should set up event listeners', function() {
+    expect(window.chrome.runtime.onInstalled.addListener).toHaveBeenCalled();
+  });
 });
